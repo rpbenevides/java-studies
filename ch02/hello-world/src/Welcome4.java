9bcae034ee9bed4ab9074ed1167483a105e80c6a
rpbenevides <rpiresbenevides@gmail.com>
2023-07-28 15:48:31 -0300

    printf

---
+++ ch02/hello-world/src/Welcome4.java
@@ -0,0 +1,9 @@
+// Figura 2.6: Welcome4.java
+// Imprimindo múltiplas linhas de texto com o método  System.out.printf
+
+public class Welcome4 {
+    // método main inicia a execução do aplicativo Java
+    public static void main(String[] args) {
+        System.out.printf("%s%n%s%n", "Welcome to", "Java Programming!");
+    } // fim do método main
+} // fim da classe Welcome3
