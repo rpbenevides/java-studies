ae3f5200aa8e31a719ce894a25ee203d1d1a457d
rpbenevides <rpiresbenevides@gmail.com>
2023-07-28 15:17:38 -0300

    múltiplas linhas de texto  (\n)

---
+++ ch02/hello-world/src/Welcome3.java
@@ -0,0 +1,9 @@
+// Figura 2.4: Welcome3.java
+// Imprimindo múltiplas linhas de texto com uma única instrução.
+
+public class Welcome3 {
+    // método main inicia a execução do aplicativo Java
+    public static void main(String[] args) {
+        System.out.println("Welcome\nto\nJava\nProgramming!");
+    } // fim do método main
+} // fim da classe Welcome3
